f4bf3fb228bebb8c80aa1f7da8e3e2a4205303ed
rakeshboyiri <boyirirakesh@gmail.com>
2023-02-03 21:27:04 +0530

    04

---
+++ assignment/04.c
@@ -0,0 +1,23 @@
+/*
+    Write a C program to check whether a character is an alphabet, digit or special character. 
+*/
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a charecter : ");
+    scanf("%c",&ch);
+    if(ch>='A' && ch<='Z' || ch>='a' && ch<='z')
+    {
+        printf("Its is a alphabet\n");
+    }
+    else if(ch>'0' && ch<'9')
+    {
+        printf("It is a digit\n");
+    }
+    else
+    {
+        printf("It is a special charecter\n");
+    }
+   
+}
