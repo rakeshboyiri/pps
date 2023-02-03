7fccf0bcaa6adaa97b012bcdb757eeefaa1e2016
rakeshboyiri <boyirirakesh@gmail.com>
2023-02-04 00:55:09 +0530

    07

---
+++ ppslab/expressionevaluation/07quadratic.c
@@ -0,0 +1,38 @@
+/*
+    Write a C program to find the roots of a Quadratic equation.
+*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;
+    float d,r1,r2;
+    printf("Enter a , b , c : ");
+    scanf("%d%d%d",&a,&b,&c);
+    d=pow(b*b-4*a*c,0.5);
+    if(d==0)
+    {
+        printf("\nReal and Equal roots");
+        r1=(-b+d)/2*a;
+        r2=(-b-d)/2*a;
+        printf("\n%f",r1);
+        printf("\n%f",r2);
+    }
+    else if(d>0)
+    {
+        printf("\nReal and not equal roots");
+        r1=(-b+d)/2*a;
+        r2=(-b-d)/2*a;
+        printf("\n%f",r1);
+        printf("\n%f",r2);
+    }
+    else
+    {
+        printf("Imaginary roots");
+        r1=(-b+d)/2*a;
+        r2=(-b-d)/2*a;
+        printf("\n%f",r1);
+        printf("\n%f",r2);
+    }
+    
+}
