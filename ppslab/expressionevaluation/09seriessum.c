7cc930690612ab01036f3c80d9096093a0fd911f
rakeshboyiri <boyirirakesh@gmail.com>
2023-02-04 01:32:18 +0530

    09

---
+++ ppslab/expressionevaluation/09seriessum.c
@@ -0,0 +1,18 @@
+/*
+    Write a C program to read in two numbers, x and n, and then compute the sum of this geometric progression: 1+x+x^2+x^3+	+x^n. For example: if n is 3 and x is 5, then the program
+    computes 1+5+25+125.
+
+*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x,n,i,sum=1;
+    printf("Enter X and N value : ");
+    scanf("%d%d",&x,&n);
+    for(i=1;i<=n;i++)
+    {
+        sum+=pow(x,i);
+    }
+    printf("Sum=%d",sum);
+}
