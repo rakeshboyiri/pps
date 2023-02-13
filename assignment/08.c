2324564bde14fca6f0c6900482ac5339b766792c
rakeshboyiri <boyirirakesh@gmail.com>
2023-02-14 01:04:52 +0530

    .

--- assignment/08.c
+++ assignment/08.c
@@ -4,24 +4,36 @@
 #include<stdio.h>
 #include<math.h>
 int amstrong();
+int count();
 int main()
 {
-    int n,i;
+    int n,i,x;
     printf("Enter a number : ");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        amstrong(i);
+    	x=count(i);
+        amstrong(i,x);
      
     }
 }
-int amstrong(int x)
+int count(int x)
+{
+	int count=0;
+	while(x!=0)
+	{
+		count++;
+		x/=10;	
+	}
+	return count;
+}
+int amstrong(int x,int c)
 {
     int rem,s=0,num=x;
     while(x!=0)
     {
         rem=x%10;      
-        s=s+pow(rem,3);
+        s=s+pow(rem,c);
         x=x/10;
     }
     if(s==num)
