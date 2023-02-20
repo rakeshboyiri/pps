e80a167c29e4de3a96a92af776d51a4d9d4827db
rakeshboyiri <boyirirakesh@gmail.com>
2023-02-20 23:24:34 +0530

    03

--- assignment/03.c
+++ assignment/03.c
@@ -10,10 +10,10 @@ int main()
     scanf("%d",&year);
     if(year%4==0 && year%100!=0 || year%400==0)
     {
-        printf("%d is leap year");
+        printf("%d is leap year",year);
     }
     else
     {
-        printf("%d is not a leap year");
+        printf("%d is not a leap year",year);
     }
 }
