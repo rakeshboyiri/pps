d3351f53e0bc660bbd4e93e996406fba7fde23bd
rakeshboyiri <boyirirakesh@gmail.com>
2023-02-21 09:24:12 +0530

    04

--- assignment/04.c
+++ assignment/04.c
@@ -11,7 +11,7 @@ void main()
     {
         printf("Its is a alphabet\n");
     }
-    else if(ch>='0' && ch=<'9')
+    else if(ch>='0' && ch<='9')
     {
         printf("It is a digit\n");
     }
@@ -19,4 +19,5 @@ void main()
     {
         printf("It is a special character\n");
     }
+}
    
